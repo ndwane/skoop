69ede308b6c5198fbda2c4ca5aeaba810fc7ebc5
Khaled <ndwanek@gmail.com>
2026-05-27 10:24:42 -0400

    Step 6: Add forgot-password screen

--- skoop-app/app/_layout.tsx
+++ skoop-app/app/_layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout() {
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="login" options={{ headerShown: false }} />
           <Stack.Screen name="signup" options={{ headerShown: false }} />
+          <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
         </Stack>
       </SafeAreaProvider>
     </AuthProvider>
